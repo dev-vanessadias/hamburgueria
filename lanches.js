9f9910add5a135e73935b64f397e389446cab9c9
dev-vanessadias <dev.vanessadias@gmail.com>
2023-01-31 19:51:17 -0300

    envio de projeto

---
+++ lanches.js
@@ -0,0 +1,83 @@
+
+let lanchesJson = [
+{
+    id: 1,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 2,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 3,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 4,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 5,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 6,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 7,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 8,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 9,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'moloh etc',
+},
+
+{
+    id: 10,
+    name: 'big-max',
+    img: 'img/lanche1.png',
+    price: 25.00,
+    desciption: 'molho etc',
+},
+
+]
---
+++ main.js
@@ -0,0 +1,8 @@
+lanchesJson.map((item, index) => {
+    //console.log(item)
+
+    let lanchesItem = document.querySelector('.cardapio .lanche-item').cloneNode(true)
+
+    document.querySelector('')
+
+})
